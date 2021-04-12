854277db810e88aa226341ee1bb10d047750e2d0
Tomas Hvozdovic <thvozdovic@gmail.com>
2021-04-12 21:05:45 +0200

    update

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="absolute bottom-0 w-screen h-16 flex justify-evenly items-center text-sm bg-gray-100 text-gray-500"
+  >
+    <router-link to="/">Home</router-link>
+    <router-link to="/sensors">Public sensors</router-link>
+    <router-link to="/notifications">Notifications</router-link>
+    <router-link to="/settings">Settings</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+router-link {
+  @apply p-2;
+}
+.router-link-active {
+  @apply text-primary;
+}
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="min-h-screen w-screen flex flex-col items-center bg-waves bg-bottom bg-cover bg-no-repeat"
+  >
+    <img src="logo.svg" alt="logo" class="w-36 mt-4" />
+    <h1 class="mt-4 text-secondary">404</h1>
+    <h2 class="mb-4">Couldn't find {{ this.route }}</h2>
+    <router-link
+      to="/"
+      class="px-3 py-2 rounded-xl text-white bg-primary shadow-sm"
+      >Back to home</router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+  data: function () {
+    return {
+      route: "",
+    };
+  },
+  mounted() {
+    this.route = this.$route.path;
+  },
+};
+</script>
